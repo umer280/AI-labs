f3510f4588184e695ee3f4e0d24065547da183f1
umer280 <muhumer503@gmail.com>
2025-01-08 22:36:27 +0500

    Add files via upload

---
+++ lab 5 part 2/task2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Step 1: Prepare the Dataset
+areas = np.array([2600, 3000, 3200, 3600, 4000]).reshape(-1, 1)
+prices = np.array([550000, 565000, 610000, 680000, 725000])
+
+# Step 2: Create and Train the Linear Regression Model
+model = LinearRegression()
+model.fit(areas, prices)
+
+# Step 3: Predict Prices for Specific Areas
+new_areas = np.array([5000, 8000, 9000]).reshape(-1, 1)
+predicted_prices = model.predict(new_areas)
+
+# Step 4: Display the Results
+for area, price in zip(new_areas, predicted_prices):
+    print(f"Area: {area[0]} sqr ft, Predicted Price: ${price:.2f}")
+
+
+# In[ ]:
+
+
+
+
